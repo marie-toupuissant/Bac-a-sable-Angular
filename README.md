196c10e66a44d6eb9543e9304d75d21822a89ead
marie-toupuissant <74815915+marie-toupuissant@users.noreply.github.com>
2022-09-04 22:34:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-uqf5du
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-uqf5du)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: Product[] = [];
+
+  // Méthode qui ajoute un produit à un tableau items
+  addToCard(product: Product) {
+    this.items.push(product);
+  }
+
+  // Méthode qui collecte les articles que les utilisateurs ajoutent au panier et renvoie caque article avec sa quantité associée
+  getItems() {
+    return this.items;
+  }
+  
+  //Méthode qui renvoie un tableau vide d'éléments ce qui vide le panier
+  clearCart(){
+    this.items = [];
+    return.this.items;
+  }
+
+
+  constructor() { }
+}
