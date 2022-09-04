c61476051a9cf81a7c28d475d005be0520e30cdf
marie-toupuissant <74815915+marie-toupuissant@users.noreply.github.com>
2022-09-05 00:33:17 +0200

    4 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -8,19 +8,19 @@ export class CartService {
   items: Product[] = [];
 
   // Méthode qui ajoute un produit à un tableau items
-  addToCard(product: Product) {
+  addToCart(product: Product) {
     this.items.push(product);
   }
 
-  // Méthode qui collecte les articles que les utilisateurs ajoutent au panier et renvoie caque article avec sa quantité associée
+  // Méthode qui collecte les articles que les utilisateurs ajoutent au panier et renvoie chaque article avec sa quantité associée
   getItems() {
     return this.items;
   }
   
   //Méthode qui renvoie un tableau vide d'éléments ce qui vide le panier
-  clearCart(){
+  clearCart() {
     this.items = [];
-    return.this.items;
+    return this.items;
   }
 
 
